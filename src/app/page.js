3001fbb3709ea70dbbccc4b6f1b2cbf7bf18b6d3
Yehea Shorafa <yeheashorafa6@gmail.com>
2025-07-12 23:50:54 +0300

    adding card list

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import AppAreaChart from "@/components/AppAreaChart/AppAreaChart";
 import AppBarChart from "@/components/AppBarChart/AppBarChart";
-import { Button } from "@/components/ui/button";
-import { Mouse } from "lucide-react";
+import AppPieChart from "@/components/AppPieChart/AppPieChart";
+import CardList from "@/components/CardList/CardList";
 
 function Home() {
   return (
@@ -9,9 +9,15 @@ function Home() {
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
         <AppBarChart />
       </div>
-      <div className="bg-primary-foreground p-4 rounded-lg"></div>
-      <div className="bg-primary-foreground p-4 rounded-lg">test</div>
-      <div className="bg-primary-foreground p-4 rounded-lg">test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <CardList title={"Popular Content"} />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <AppPieChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <CardList title={"latest Transactions"} />
+      </div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
         <AppAreaChart />
       </div>
---
+++ src/components/AppPieChart/AppPieChart.jsx
@@ -0,0 +1,130 @@
+"use client";
+
+import * as React from "react";
+import { TrendingUp } from "lucide-react";
+import { Label, Pie, PieChart } from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+export const description = "A donut chart with text";
+
+const chartData = [
+  { browser: "chrome", visitors: 275, fill: "var(--chart-1)" },
+  { browser: "safari", visitors: 200, fill: "var(--chart-2)" },
+  { browser: "firefox", visitors: 287, fill: "var(--chart-3)" },
+  { browser: "edge", visitors: 173, fill: "var(--chart-4)" },
+  { browser: "other", visitors: 190, fill: "var(--chart-5)" },
+];
+
+const chartConfig = {
+  visitors: {
+    label: "Visitors",
+  },
+  chrome: {
+    label: "Chrome",
+    color: "var(--chart-1)",
+  },
+  safari: {
+    label: "Safari",
+    color: "var(--chart-2)",
+  },
+  firefox: {
+    label: "Firefox",
+    color: "var(--chart-3)",
+  },
+  edge: {
+    label: "Edge",
+    color: "var(--chart-4)",
+  },
+  other: {
+    label: "Other",
+    color: "var(--chart-5)",
+  },
+};
+
+function AppPieChart() {
+  const totalVisitors = chartData.reduce((acc, curr) => acc + curr.visitors, 0);
+
+  return (
+    <div>
+      <Card className="flex flex-col">
+        <CardHeader className="items-center pb-0">
+          <CardTitle>Website Visitors Overview</CardTitle>
+          <CardDescription>Data from January to June 2024</CardDescription>
+        </CardHeader>
+        <CardContent className="flex-1 pb-0">
+          <ChartContainer
+            config={chartConfig}
+            className="mx-auto aspect-square max-h-[250px]"
+          >
+            <PieChart>
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent hideLabel />}
+              />
+              <Pie
+                data={chartData}
+                dataKey="visitors"
+                nameKey="browser"
+                innerRadius={60}
+                strokeWidth={5}
+              >
+                <Label
+                  content={({ viewBox }) => {
+                    if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                      return (
+                        <text
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          textAnchor="middle"
+                          dominantBaseline="middle"
+                        >
+                          <tspan
+                            x={viewBox.cx}
+                            y={viewBox.cy}
+                            className="fill-foreground text-3xl font-bold"
+                          >
+                            {totalVisitors.toLocaleString()}
+                          </tspan>
+                          <tspan
+                            x={viewBox.cx}
+                            y={(viewBox.cy || 0) + 24}
+                            className="fill-muted-foreground"
+                          >
+                            Total Visitors
+                          </tspan>
+                        </text>
+                      );
+                    }
+                  }}
+                />
+              </Pie>
+            </PieChart>
+          </ChartContainer>
+        </CardContent>
+        <CardFooter className="flex-col gap-2 text-sm">
+          <div className="flex items-center gap-2 leading-none font-medium">
+            Traffic increased by 5.2% this month <TrendingUp className="h-4 w-4" />
+          </div>
+          <div className="text-muted-foreground leading-none">
+            Aggregated visitor data from the past 6 months
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
+
+export default AppPieChart;
